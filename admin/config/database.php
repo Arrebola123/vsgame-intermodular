d6326c1b363cc8daa68c6e49bf23abf6b2a9585a
Alejandro Sánchez Ruiz <alexsanchezgradomedio@gmail.com>
2025-12-03 13:23:04 +0100

    Configuración de la Base De Datos cambiada para funcionar en servidor web

--- admin/config/database.php
+++ admin/config/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static $host = 'localhost';
     public static $nombre_bd = 'vsgame';
-    public static $usuario = 'root';
-    public static $password = '';
+    public static $usuario = 'vsgame_user';
+    public static $password = '1234';
 }
 ?>
